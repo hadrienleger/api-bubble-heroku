303b89540a88b0e539e2c2c630a745f2402656f8
Hadrien Leger <legerhad@gmail.com>
2025-01-13 21:11:17 +0100

    Correction de valeur_va_figaro par niveau_colleges_figaro

--- api-bubble-heroku.js
+++ api-bubble-heroku.js
@@ -328,7 +328,7 @@ app.post('/get_carreaux_filtre', async (req, res) => {
     // ----------------------------------------------------
     // F) Critère collèges => criteria.colleges
     //    table pivot : idcar200m_rne_niveaucolleges
-    //    champ valeur_avec_va_figaro
+    //    champ niveau_colleges_figaro
     // ----------------------------------------------------
     if (criteria && criteria.colleges) {
       console.time('F) Colleges');
@@ -337,12 +337,12 @@ app.post('/get_carreaux_filtre', async (req, res) => {
       let ic = 1;
 
       if (criteria.colleges.valeur_figaro_min != null) {
-        wf.push(`valeur_avec_va_figaro >= $${ic}`);
+        wf.push(`niveau_colleges_figaro >= $${ic}`);
         vf.push(criteria.colleges.valeur_figaro_min);
         ic++;
       }
       if (criteria.colleges.valeur_figaro_max != null) {
-        wf.push(`valeur_avec_va_figaro <= $${ic}`);
+        wf.push(`niveau_colleges_figaro <= $${ic}`);
         vf.push(criteria.colleges.valeur_figaro_max);
         ic++;
       }
